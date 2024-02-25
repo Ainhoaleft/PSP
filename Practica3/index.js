319543e1c646c2996cefd308ec36e1067327e039
Ainhoaleft <izquierdo19668@iesmarenostrum.com>
2024-02-25 17:04:02 +0100

    Practica 3 Hecha

--- Practica3/index.js
+++ Practica3/index.js
@@ -2,7 +2,7 @@ const express = require("express");
 const mongoose = require("mongoose");
 const bodyParser = require('body-parser');
 
-mongoose.connect('mongodb://localhost:27017/biblioteca', { 
+mongoose.connect('mongodb://localhost:27017/FormulaUno', { 
     useNewUrlParser: true, 
     useUnifiedTopology: true 
 }); 
@@ -10,94 +10,94 @@ mongoose.connect('mongodb://localhost:27017/biblioteca', {
 let app = express();
 app.use(bodyParser.json()); 
 
-let libroSchema = new mongoose.Schema({     
-    titulo: String,     
-    autor: [String],     
-    ejemplares: Number 
-});  
+let pilotoSchema = new mongoose.Schema({     
+    escuderia: String,     
+    nombre: [String],     
+    anyos: Number 
+}); 
 
-let Libro = mongoose.model('libros', libroSchema); 
+let Piloto = mongoose.model('pilotos', pilotoSchema); 
 
-app.get('/libros', (req, res) => {     
-    Libro.find().then(result => {         
+//A. Obtener todos los documentos de la colección.
+app.get('/pilotos', (req, res) => {     
+    Piloto.find().then(result => {         
         res.send(result);     
     })
 });
 
-app.post('/libros', async (req, res) => {
-    try {
-      const librosData = Array.isArray(req.body) ? req.body : [req.body];
-      const libros = await Libro.insertMany(librosData);
-      res.status(201).json(libros);
-    } catch (error) {
-      res.status(500).json({ error: error.message });
-    }
-  });
-
-
-// C. Obtener todos los documentos que cumplan dos condiciones
-app.get('/libros/busqueda', (req, res) => {
-    let query = {};
-    if (req.query.autor) {
-        query.autor = req.query.autor;
-    }
-    if (req.query.minEjemplares) {
-        query.ejemplares = { $gte: Number(req.query.minEjemplares) };
-    }
-
-    Libro.find(query)
-        .then(libros => {
-            res.status(200).json(libros);
-        })
-        .catch(error => {
-            res.status(500).json({ error: error.message });
-    });
-});
 // B. Obtener un documento por su ID
-app.get('/libros/:id', (req, res) => {     
-    Libro.findById(req.params.id)
-        .then(libro => {
-            if (!libro) {
-                return res.status(404).json({ message: 'Libro no encontrado' });
+app.get('/pilotos/:id', (req, res) => {     
+    Piloto.findById(req.params.id)
+        .then(Piloto => {
+            if (!Piloto) {
+                return res.status(404).json({ message: 'Piloto no encontrado' });
             }
-            res.status(200).json(libro);
+            res.status(200).json(Piloto);
         })
         .catch(error => {
             res.status(500).json({ error: error.message });
         });
 });
-  
+
+// C. Obtener todos los documentos que cumplan dos condiciones
+app.get('/pilotos/busqueda', (req, res) => {
+    let query = {};
+    if (req.query.nombre) {
+        query.nombre = req.query.nombre;
+    }
+    if (req.query.minAnyos) {
+        query.anyos = { $gte: Number(req.query.minAnyos) };
+    }
+
+    Piloto.find(query)
+        .then(Pilotos => {
+            res.status(200).json(Pilotos);
+        })
+        .catch(error => {
+            res.status(500).json({ error: error.message });
+    });
+});
+
+//D. Insertar uno o varios documentos.
+app.post('/pilotos', async (req, res) => {
+    try {
+      const pilotosData = Array.isArray(req.body) ? req.body : [req.body];
+      const pilotos = await Piloto.insertMany(pilotosData);
+      res.status(201).json(pilotos);
+    } catch (error) {
+      res.status(500).json({ error: error.message });
+    }
+});
+
     // E. Actualizar un documento dada una condición
-    app.put('/libros', (req, res) => {
-        const { titulo, ejemplares } = req.body;
-        Libro.findOneAndUpdate({ titulo: titulo }, { $set: { ejemplares: ejemplares } }, { new: true })
-            .then(libro => {
-                if (!libro) {
-                    return res.status(404).json({ message: 'Libro no encontrado' });
+    app.put('/pilotos', (req, res) => {
+        const { escuderia, anyos } = req.body;
+        Piloto.findOneAndUpdate({ escuderia: escuderia }, { $set: { anyos: anyos } }, { new: true })
+            .then(Piloto => {
+                if (!Piloto) {
+                    return res.status(404).json({ message: 'Piloto no encontrado' });
                 }
-                res.status(200).json(libro);
+                res.status(200).json(Piloto);
             })
             .catch(error => {
                 res.status(500).json({ error: error.message });
             });
     });
-    
-    
+     
     // F. Eliminar un documento dado su id
-    app.delete('/libros/:id', (req, res) => {
-        Libro.findOneAndDelete({ _id: req.params.id })
+    app.delete('/pilotos/:id', (req, res) => {
+        Piloto.findOneAndDelete({ _id: req.params.id })
             .then(result => {
                 if (!result) {
-                    return res.status(404).json({ message: 'Libro no encontrado' });
+                    return res.status(404).json({ message: 'Piloto no encontrado' });
                 }
-                res.status(200).json({ message: 'Libro eliminado correctamente' });
+                res.status(200).json({ message: 'Piloto eliminado correctamente' });
             })
             .catch(error => {
                 res.status(500).json({ error: error.message });
             });
     });
     
-
 app.listen(8080, () => {
-    console.log("Servidor escuchando en el puerto 8080");
+    console.log("Se escucha en el puerto 8080");
 });
